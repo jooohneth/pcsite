b3baecb7162c023a5075d4efde4b1d40391e646a
jooohneth <jooohn.eth@gmail.com>
2025-04-15 22:02:38 -0400

    feat: add items

---
+++ frontend/src/components/order/items.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import type { PCPart } from "../pc-card";
+import { Cpu } from "lucide-react";
+import { Badge } from "../ui/badge";
+
+interface ItemProps {
+  id: string;
+}
+
+const Item = ({ id }: ItemProps) => {
+  const [itemInfo, setItemInfo] = useState<PCPart | null>(null);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/api/parts/${id}/`, {
+          method: "GET",
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch item");
+        }
+
+        const data = await response.json();
+        setItemInfo(data);
+      } catch (error) {
+        console.error("Error fetching item:", error);
+        return null;
+      }
+    };
+
+    fetchItem();
+  }, []);
+
+  return (
+    <div className="flex-1 min-w-0 py-4">
+      <div className="flex justify-between">
+        <div>
+          <h4 className="pl-2 font-bold text-md truncate">{itemInfo?.name}</h4>
+          <p className="pl-2 text-muted-foreground">{itemInfo?.manufacturer}</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <Badge
+              variant="outline"
+              className="text-sm font-bold text-blue-500 border-transparent bg-black shadow-xl"
+            >
+              <Cpu className="h-5 w-5" />
+              <span className="ml-1">{itemInfo?.type}</span>
+            </Badge>
+          </div>
+
+          <p className="pl-2 text-green-500 font-bold">${itemInfo?.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
